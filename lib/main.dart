4fcd071c0db32b62ed4f6af9b6806470a782b3ee
Zaher ABBAS <z.abbas83@yahoo.com>
2025-10-01 15:22:28 +0200

    feat: Implement basic HomePage UI
    This commit implements the basic user interface for the `HomePage` widget.
    
    Key changes:
    - Added a `Scaffold` to `HomePage` with an `AppBar` displaying the title 'WildSnap'.
    - Set the body of the `Scaffold` to display a centered text 'Welcome to WildSnap!'.
    - Updated the `MaterialApp` theme in `main.dart` to set the `scaffoldBackgroundColor` to green.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'WildSnap',
       theme: ThemeData(
+        scaffoldBackgroundColor: (Colors.green),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const HomePage(),
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,6 +5,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('WildSnap')),
+      body: const Center(child: Text('Welcome to WildSnap!')),
+    );
   }
 }
