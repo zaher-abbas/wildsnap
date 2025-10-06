d1407e6f75190c9b7f76f945f5e5b7020950c8e7
Ethan <e.boulard07@gmail.com>
2025-10-06 11:42:01 +0200

    fix: add a condition to protect from the dynamic type

--- lib/models/cat_facts.dart
+++ lib/models/cat_facts.dart
@@ -1,10 +1,16 @@
 class CatFact {
   final String fact;
 
-  CatFact({ required this.fact });
+  CatFact({required this.fact});
 
   factory CatFact.fromJson(Map<String, dynamic> json) {
-    final List<dynamic> data = json['data'];
-    return CatFact(fact: data.first.toString());
+    final data = json['data'];
+
+    // si data est une liste d'élément et data n'est pas vide et que data est une chaine de caractère alors :
+    if (data is List && data.isNotEmpty && data.first is String) {
+      // convertis le premier element de type dynamic en chaine de caractère
+      return CatFact(fact: data.first as String);
+    }
+    throw Exception('Invalid JSON: "data" should be a non-empty List<String>');
   }
 }
